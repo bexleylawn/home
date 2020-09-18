bdcb5a3db8942f789f9082362fe8854d2a30e577
harvey67 <67565077+harvey67@users.noreply.github.com>
2020-09-18 12:44:36 +1000

    Updates

---
+++ precache-manifest.e33ba8c8f05354deeeaf2edc535aece0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5165af30784f2452a315eabe7f19ea5a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "53e3ad078734d1f7c1b5",
+    "url": "/home/static/css/main.147cffb6.chunk.css"
+  },
+  {
+    "revision": "50eb66501763a1d8e90f",
+    "url": "/home/static/js/2.74f0a912.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/home/static/js/2.74f0a912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e3ad078734d1f7c1b5",
+    "url": "/home/static/js/main.9f996b6a.chunk.js"
+  },
+  {
+    "revision": "327519257ae83559732d",
+    "url": "/home/static/js/runtime-main.1ebb12cf.js"
+  },
+  {
+    "revision": "7c4701ba25cf54e299dd97291c786b33",
+    "url": "/home/static/media/background1.7c4701ba.jpg"
+  },
+  {
+    "revision": "bc94c9abe8d768bc4fd78a9e96a750aa",
+    "url": "/home/static/media/background2.bc94c9ab.jpg"
+  },
+  {
+    "revision": "4bece755dc2010455cd41096c4799a3f",
+    "url": "/home/static/media/background3.4bece755.jpg"
+  },
+  {
+    "revision": "16ceae34bb8c8b23c65550efc9b95d05",
+    "url": "/home/static/media/logo.16ceae34.jpg"
+  },
+  {
+    "revision": "9979ed0bd69f16380cc8ce76ce05843d",
+    "url": "/home/static/media/p1.9979ed0b.jpg"
+  },
+  {
+    "revision": "1ed91f931d56b0c8349f160775192129",
+    "url": "/home/static/media/p3.1ed91f93.jpg"
+  },
+  {
+    "revision": "65e4553db1d360624b3a3a07fe33afcc",
+    "url": "/home/static/media/p4.65e4553d.jpg"
+  },
+  {
+    "revision": "120b54aeea511463fde04712cd419fa9",
+    "url": "/home/static/media/p5.120b54ae.jpg"
+  },
+  {
+    "revision": "cacdbdd530282d50002be361930cf491",
+    "url": "/home/static/media/p6.cacdbdd5.jpg"
+  },
+  {
+    "revision": "72335e25d203f833e9d32591acee0b2a",
+    "url": "/home/static/media/p7.72335e25.jpg"
+  },
+  {
+    "revision": "26a53b27aee06a01a090f0266f65bca5",
+    "url": "/home/static/media/p8.26a53b27.jpg"
+  }
+]);
